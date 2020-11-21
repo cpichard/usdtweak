766a168746b8737ca3c6930456e7a925429dbc5c
cyril <pichard.cyril@laposte.net>
2020-11-21 15:22:16 +0000

    fix potential division by zero

--- src/GeometricFunctions.h
+++ src/GeometricFunctions.h
@@ -8,21 +8,23 @@ PXR_NAMESPACE_USING_DIRECTIVE
 /// Check if all components of v1 and v2 have the same signs
 bool SameSign(const GfVec2d &v1, const GfVec2d &v2) { return v1[0] * v2[0] >= 0.0 && v1[1] * v2[1] >= 0.0; }
 
-/// Given the segment defined by 2 points segP2 and segP1, check if point is within limits from the segment
-bool PickSegment(const GfVec2d &segP1, const GfVec2d &segP2, const GfVec2d &point, const GfVec2d &limits) {
+/// Given the segment defined by 2 points p1 and p2, check if point is within boundaries from the segment
+bool PickSegment(const GfVec2d &p1, const GfVec2d &p2, const GfVec2d &point, const GfVec2d &limits) {
     // Line vector
-    const GfVec2d segment(segP1 - segP2);
+    const GfVec2d segment(p1 - p2);
 
     // Point clicked as a 2d point
-    const auto projectionOnSegment = (point - segP2).GetProjection(segment.GetNormalized());
-
-    const auto ratio = projectionOnSegment.GetLength() / segment.GetLength();
-    if (ratio > 0.0 && ratio <= 1.0 && SameSign(projectionOnSegment, segment)) {
-        GfVec2d dist(projectionOnSegment - point + segP2);
-        if (fabs(dist[0]) < limits[0] && fabs(dist[1]) < limits[1]) {
-            return true;
+    const auto projectionOnSegment = (point - p2).GetProjection(segment.GetNormalized());
+    if (segment.GetLength()){
+        const auto ratio = projectionOnSegment.GetLength() / segment.GetLength();
+        if (ratio > 0.0 && ratio <= 1.0 && SameSign(projectionOnSegment, segment)) {
+            GfVec2d dist(projectionOnSegment - point + p2);
+            if (fabs(dist[0]) < limits[0] && fabs(dist[1]) < limits[1]) {
+                return true;
+            }
         }
     }
+
     return false;
 }
 
@@ -31,3 +33,4 @@ GfVec2d ProjectToNormalizedScreen(const GfMatrix4d &mv, const GfMatrix4d &proj, 
     auto projected = proj.Transform(mv.Transform(point));
     return GfVec2d(projected[0], projected[1]);
 }
+
