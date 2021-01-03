e84a7d2052fe93ce005654bf5e4f1bd5e97634a2
cpichard <cpichard.github@gmail.com>
2021-01-03 19:05:50 +0000

    add OnSelectionChange to the manipulator base class + reformat

--- src/viewport/Manipulator.h
+++ src/viewport/Manipulator.h
@@ -2,18 +2,23 @@
 
 class Viewport;
 
-/// Editing state -> this could be embedded in the manipulator ???
-/// TODO: the name of the base class for a manipulator/editor/gizmo is confusing, find a better name
-///        also it needs a bit of documentation now
+/// Base class for a manipulator. The manipulator is multiple thing at the same time, it is seen as an editing state from a fsm
+/// as a portion of code which renders a gizmo,
+
+/// It forces to override the function OnUpdate which is called for every rendered frame.
+/// The manipulator should return the
+
 struct Manipulator {
-    virtual ~Manipulator() {};
-    virtual void OnBeginEdition(Viewport &) {}; // Enter State
-    virtual void OnEndEdition(Viewport &) {};  // Exit State
-    virtual Manipulator *  OnUpdate(Viewport &) = 0;
+    virtual ~Manipulator(){};
+    virtual void OnBeginEdition(Viewport &){}; // Enter State
+    virtual void OnEndEdition(Viewport &){};   // Exit State
+    virtual Manipulator *OnUpdate(Viewport &) = 0; // Next State
 
     virtual bool IsMouseOver(const Viewport &) { return false; };
 
     /// Draw the translate manipulator as seen in the viewport
-    virtual void OnDrawFrame(const Viewport &) {};
+    virtual void OnDrawFrame(const Viewport &){};
 
+    /// Called when the viewport changes its selection
+    virtual void OnSelectionChange(Viewport &){};
 };
