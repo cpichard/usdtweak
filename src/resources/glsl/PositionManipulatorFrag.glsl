4aa6266feb3a43da9c336acc6a0c3fbde80dc68c
cpichard <cpichard.github@gmail.com>
2021-03-20 10:31:31 +0000

    reformat glsl code

--- src/resources/glsl/PositionManipulatorFrag.glsl
+++ src/resources/glsl/PositionManipulatorFrag.glsl
@@ -2,9 +2,12 @@
 in vec4 color;
 out vec4 FragColor;
 uniform vec3 highlight;
-void main()
-{
-    if (dot(highlight, color.xyz) >0.9) 
-       { FragColor = vec4(1.0, 1.0, 0.2, 1.0);}
-  else {  FragColor = color; }
+
+void main() {
+    // if (color.a == 0.0) discard;
+    if (dot(highlight, color.xyz) > 0.9) {
+       FragColor = vec4(1.0, 1.0, 0.2, color.w);
+    } else {
+       FragColor = color;
+    }
 };
--- src/resources/glsl/PositionManipulatorVert.glsl
+++ src/resources/glsl/PositionManipulatorVert.glsl
@@ -6,9 +6,15 @@ uniform mat4 modelView;
 uniform mat4 projection;
 uniform mat4 objectMatrix;
 out vec4 color;
-void main()
-{
+
+void main() {
     vec4 bPos = objectMatrix*vec4(aPos*scale, 1.0);
     gl_Position = projection*modelView*bPos;
     color = inColor;
+// For the rotation circles, hide the parts that should be hidden if the manipulator was a sphere
+//	vec4 ori = objectMatrix*vec4(0.0, 0.0, 0.0, 1.0);
+//	vec4 cam = inverse(modelView)*vec4(0.0, 0.0, 0.0, 1.0);
+//	if(dot((ori-cam), (ori-bPos)) < 0.0){
+//		color.a = 0.0;
+//	}
 }
