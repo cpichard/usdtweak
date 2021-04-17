b28f307dd4f9e9c508ca97abef14cc8ec7040ce1
cpichard <cpichard.github@gmail.com>
2021-04-17 10:07:30 +0100

    remove useless comments

--- src/resources/ResourcesLoader.cpp
+++ src/resources/ResourcesLoader.cpp
@@ -6,17 +6,7 @@ ResourcesLoader::ResourcesLoader() {
     // Font
     ImGuiIO &io = ImGui::GetIO();
     ImFontConfig fontConfig;
-   // fontConfig.SizePixels = 13;
- // fontConfig.MergeMode = true;
-   // fontConfig.GlyphMinAdvanceX = 18.0f; // icon monospaced
     auto fontDefault = io.Fonts->AddFontDefault(&fontConfig); // DroidSans
- //   auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\DroidSans.ttf", 18.0f,  &fontConfig);
-   // auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\Roboto-Medium.ttf", 16.0f,  &fontConfig);
-    //auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\ProggyClean.ttf", 13.0f,  &fontConfig);
-    //auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\ProggyTiny.ttf", 10.0f,  &fontConfig);
-    //auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\Cousine-Regular.ttf", 18.0f,  &fontConfig);
-    //auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\Asimov.ttf", 18.0f,  &fontConfig);
-    //auto fontDefault = io.Fonts->AddFontFromFileTTF("C:\\\\Users\\\\cpich\\\\installs\\\\imgui\\\\misc\\\\fonts\\\\SourceCodePro-Regular.ttf", 12.0f,  &fontConfig);
 
     // Icons (in font)
     static const ImWchar iconRanges[] = {ICON_MIN_FA, ICON_MAX_FA, 0};
