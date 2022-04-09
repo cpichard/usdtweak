344812053a679052a55536a8fc3e71212218ce00
cpichard <cpichard.github@gmail.com>
2022-04-09 10:00:41 +0100

    change the application style to dark mode (experimental)

--- src/widgets/TextEditor.cpp
+++ src/widgets/TextEditor.cpp
@@ -1,6 +1,7 @@
 #include "TextEditor.h"
-#include "Gui.h"
 #include "Commands.h"
+#include "Gui.h"
+#include "ImGuiHelpers.h"
 
 // The following include contains the code which writes usd to text, but it's not
 // distributed with the api
@@ -8,6 +9,7 @@
 
 void DrawTextEditor(SdfLayerRefPtr layer) {
     static std::string layerText;
+    ImGuiIO &io = ImGui::GetIO();
     ImGui::Text("WARNING: this will slow down the application if the layer is big");
     ImGui::Text("        and will consume lots of memory. Use with care for now");
     if (layer) {
@@ -16,12 +18,17 @@ void DrawTextEditor(SdfLayerRefPtr layer) {
     }
     ImGui::PushItemWidth(-FLT_MIN);
     ImGuiWindow *currentWindow = ImGui::GetCurrentWindow();
-    ImVec2 sizeArg(0, currentWindow->Size[1] - 110);
-
-    ImGui::InputTextMultiline("###TextEditor", &layerText, sizeArg, ImGuiInputTextFlags_None | ImGuiInputTextFlags_NoUndoRedo);
-    if (layer && ImGui::IsItemDeactivatedAfterEdit()) {
-        ExecuteAfterDraw<LayerTextEdit>(layer, layerText);
+    ImVec2 sizeArg(0, currentWindow->Size[1] - 120);
+    ImGui::PushFont(io.Fonts->Fonts[1]);
+    {
+        ScopedStyleColor color(ImGuiCol_FrameBg, ImVec4{0.0, 0.0, 0.0, 1.0});
+        ImGui::InputTextMultiline("###TextEditor", &layerText, sizeArg,
+                                  ImGuiInputTextFlags_None | ImGuiInputTextFlags_NoUndoRedo);
+        if (layer && ImGui::IsItemDeactivatedAfterEdit()) {
+            ExecuteAfterDraw<LayerTextEdit>(layer, layerText);
+        }
     }
+    ImGui::PopFont();
     ImGui::PopItemWidth();
     ImGui::Text("Ctrl+Enter to apply your change");
 }
