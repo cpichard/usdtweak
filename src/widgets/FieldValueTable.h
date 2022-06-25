ac5085e3ad3586e8e32a59839a3efccb72d9b3d1
cpichard <cpichard.github@gmail.com>
2022-06-25 17:17:34 +0100

    add new table layout with 2 columns

--- src/widgets/FieldValueTable.h
+++ src/widgets/FieldValueTable.h
@@ -1,12 +1,18 @@
 #pragma once
 
-#include "Gui.h"
 
 //
-// Field and value generic table, to deduplicate the layout code used to draw the tables
+// Provide a standard and consistent layout for tables with button+field+value, and I don't have to write and fix the same code multiple time in the source
 //
+// TODO: rename to TableLayouts.h
+// and functions like TableLayoutBeginTable() ...
 
 template <typename FieldT, typename... Args> inline bool HasEdits(const Args &...args) { return true; }
+
+template <typename FieldT, typename... Args> inline ScopedStyleColor GetRowStyle(const int rowId, const Args &...args) {
+    return ScopedStyleColor(ImGuiCol_Text, HasEdits<FieldT>(args...) ? ImVec4(ColorAttributeAuthored) : ImVec4(ColorAttributeUnauthored),
+            ImGuiCol_Button, ImVec4(ColorTransparent));
+}
 
 // Default implementation
 template <typename FieldT, typename... Args> inline void DrawFieldValue(const int rowId, const Args &...args) {}
@@ -19,9 +25,7 @@ template <typename FieldT, typename... Args> inline void DrawFieldButton(const i
 
 template <typename FieldT, typename... Args> inline void DrawFieldValueTableRow(const int rowId, const Args &...args) {
     ImGui::TableNextRow(ImGuiTableRowFlags_None, TableRowDefaultHeight);
-    ScopedStyleColor hasEditColor(ImGuiCol_Text,
-                                  HasEdits<FieldT>(args...) ? ImVec4(ColorAttributeAuthored) : ImVec4(ColorAttributeUnauthored),
-                                  ImGuiCol_Button, ImVec4(ColorTransparent));
+    ScopedStyleColor style = GetRowStyle<FieldT>(rowId, args...);
     ImGui::TableSetColumnIndex(0);
     DrawFieldButton<FieldT>(rowId, args...);
     ImGui::TableSetColumnIndex(1);
@@ -41,6 +45,31 @@ inline void SetupFieldValueTableColumns(const bool showHeaders, const char *butt
                                         const char *value = "Value") {
     ImGui::TableSetupColumn(button, ImGuiTableColumnFlags_WidthFixed, 24); // 24 => size of the mini button
     ImGui::TableSetupColumn(field, ImGuiTableColumnFlags_WidthFixed);
+    ImGui::TableSetupColumn(value, ImGuiTableColumnFlags_WidthStretch);
+    if (showHeaders) {
+        ImGui::TableHeadersRow();
+    }
+}
+
+
+template <typename FieldT, typename... Args> inline void DrawValueTableRow(const int rowId, const Args &...args) {
+    ImGui::TableNextRow(ImGuiTableRowFlags_None, TableRowDefaultHeight);
+    ScopedStyleColor style = GetRowStyle<FieldT>(rowId, args...);
+    ImGui::TableSetColumnIndex(0);
+    DrawFieldButton<FieldT>(rowId, args...);
+    ImGui::TableSetColumnIndex(1);
+    DrawFieldValue<FieldT>(rowId, args...);
+}
+
+inline bool BeginValueTable(const char *strId) {
+    constexpr ImGuiTableFlags tableFlags = ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_RowBg;
+    return ImGui::BeginTable(strId, 2, tableFlags);
+}
+
+inline void EndValueTable() { ImGui::EndTable(); }
+
+inline void SetupValueTableColumns(const bool showHeaders, const char *button = "", const char *value = "Value") {
+    ImGui::TableSetupColumn(button, ImGuiTableColumnFlags_WidthFixed);
     ImGui::TableSetupColumn(value, ImGuiTableColumnFlags_WidthStretch);
     if (showHeaders) {
         ImGui::TableHeadersRow();
