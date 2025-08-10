65ab54dae1208bb93c7b573f062f9dda884934f3
cpichard <cpichard@users.noreply.github.com>
2025-08-10 09:56:04 +0100

    fix RequestFont return type

--- src/resources/ResourcesLoader.h
+++ src/resources/ResourcesLoader.h
@@ -31,9 +31,9 @@ class ResourcesLoader {
     // Set the path of the desired fonts. The font won't be loaded when calling this function, for that you have to call
     // LoadFonts. This is done this way as don't want to load the fonts when parsing the config file or when drawing. The
     // resourceLoader keeps the path to the desired fonts.
-    static bool RequestNewFontRegular(const std::string &fontPath) { _fontRegularPathRequested = fontPath; }
-    static bool RequesNewFontMono(const std::string &fontPath) { _fontMonoPathRequested = fontPath; }
-    static bool SetGlyphRangeName(const std::string &glyphName) { _glyphRange = glyphName; }
+    static void RequestNewFontRegular(const std::string &fontPath) { _fontRegularPathRequested = fontPath; }
+    static void RequesNewFontMono(const std::string &fontPath) { _fontMonoPathRequested = fontPath; }
+    static void SetGlyphRangeName(const std::string &glyphName) { _glyphRange = glyphName; }
 
     // Load regular and mono fonts, should be called outside of the main draw function using the command
     // EditorReloadFonts
